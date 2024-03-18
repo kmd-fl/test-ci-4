8f8f22f7c61d84256ee2fc13a75915fee3c5615d
Maria Kuklina <maria@fluence.one>
2024-03-18 09:59:43 +0100

    feat: update test-lib-1

--- test-lib-1/src/lib.rs
+++ test-lib-1/src/lib.rs
@@ -1,14 +1,3 @@
 pub fn add1(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add1(2, 2);
-        assert_eq!(result, 4);
-    }
+    left + right + 1
 }
